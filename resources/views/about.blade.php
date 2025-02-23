5fce8cf7f359ad8f62bd225025d6351eb1f7f60e
Joao <joao.khalaf@gmail.com>
2025-02-23 20:15:43 -0300

    like a react

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Page</title>
-</head>
-<body>
-    <h1>About Page</h1>
-</body>
-</html>
+<x-layout>
+    <h1>bla bla bla bla</h1>
+</x-layout>
