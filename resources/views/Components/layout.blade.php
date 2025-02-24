8696ddbc3599082c196f1b4181a5e172d05bb280
Joao <joao.khalaf@gmail.com>
2025-02-23 22:35:44 -0300

    nav

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="antialiased h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,16 +7,16 @@
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
 <body>
-<div class="bg-white">
+<div>
   <header>
     <nav class="flex items-center justify-around" aria-label="Global">
       <div class="flex">
         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.entoin.com%2Fimages%2Fac7.jpg&f=1&nofb=1&ipt=693839ed9ef641302f5757662c543159a66171b795ce39cf3fb2155a1b497bd5&ipo=images" alt="Logo" class="h-8 w-auto">
       </div>
       <div class="lg:flex lg:gap-x-12">
-        <a href="/" class="text-sm font-semibold text-gray-900">Home</a>
-        <a href="/about" class="text-sm font-semibold text-gray-900">About</a>
-        <a href="/contact" class="text-sm font-semibold text-gray-900">Contact</a>
+        <x-nav-link href="/" :active="request()->is('/')">Home </x-nav-link>
+        <x-nav-link href="/about" :active="request()->is('about')" >About </x-nav-link>
+        <x-nav-link href="/contact" :active="request()->is('contact')">Contact </x-nav-link>
       </div>
     </nav>   
   </header>
--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,1 +1,7 @@
-<a {{ $attributes }}> {{ $slot }}</a>
+@props(['active' => false])
+
+<a class="{{ $active ? 'bg-blue-600 text-white' : 'bg-gray-200 text-gray-800' }} text-sm font-semibold rounded-lg px-4 py-2 transition duration-300 ease-in-out transform hover:scale-105"
+    aria-current="{{ $active ? 'page' : 'false' }}"
+    {{ $attributes }}>
+     {{ $slot }}
+</a>
